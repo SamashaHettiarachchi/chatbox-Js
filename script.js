01e518ed53406d886dd28c290e4e0c91e233b6f1
Samasha Hettiarachchi <sashinisamasha@gmail.com>
2025-04-08 22:41:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const closeChatbot = document.querySelector("#close-chat");
 const uploadFileButton = document.querySelector("#upload-file-btn");
 
 // API setup - using local server
-const API_URL = "http://192.168.102.36:3000/chat";  // Use your local IP
+const API_URL = "";  // Use your local IP
 
 
 const userData = {
